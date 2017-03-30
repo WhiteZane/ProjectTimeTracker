ce54d9c7617829077cfac754d743d94b1fa54beb
WhiteZane <whi12059@byui.edu>
2017-03-30 10:22:32 -0600

    classes added for databases

---
+++ ProjectTimeTracker/Models/ActivityTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectTimeTracker.Models
+{
+    public class ActivityTypes
+    {
+        
+
+        public int ActivityTypeID { get; set; }
+        public string ActTypeDescrip { get; set; }
+
+
+
+    }
+}
---
+++ ProjectTimeTracker/Models/Users.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectTimeTracker.Models
+{
+    public class Users
+    {
+        public int ID { get; set; }
+        public string FullName { get; set; }
+
+        public ICollection<TimeLog> TimeLog { get; set; }
+    }
+}
